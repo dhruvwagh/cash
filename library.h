17fb396c2e1c37d542a467c99a6e9268892e01d9
dhruv <waghdhruv@gmail.com>
2023-01-16 14:05:11 +0200

    ver2

--- library.h
+++ library.h
@@ -1,108 +1,109 @@
 #ifndef CASH_LIBRARY_H
 #define CASH_LIBRARY_H
+
 #include<iostream>
 #include<map>
 #include<memory>
 #include<any>
 #include<vector>
 #include<unordered_map>
+#include<list>
+#include <algorithm>
 
-typedef std::unordered_map<int16_t, std::pair<int16_t, std::any>> c_map;
-std::vector<std::pair<c_map *, int16_t>> key_list;
-int16_t kit;
 
-namespace Cache {
+/*namespace Cache {
     int PoolId = 0;
     static std::vector<c_map *> space;
 }
+ */
 
 //defining an item:
 
 struct Item {
     static std::any d;
-    static int16_t Key;
+    static std::string Key;
     static int16_t age;
-    static c_map *pool;
 };
+typedef std::unordered_map<std::string, Item *> c_map;
+std::map<c_map *, std::list<Item>> pool_list;
 
-c_map *create_pool() {
-    static c_map pool;
+c_map create_pool() {
+    c_map pool;
     pool.reserve(256); // reserving memory space
     c_map *poolid = &pool; // pool(map) address
-    Cache::space.push_back(poolid); // adding a pool to the memory space
-    std::pair<c_map *, int16_t> pk_pair;
-    pk_pair.first = poolid;
-    pk_pair.second = 0;
-    key_list.push_back(pk_pair);
-    return poolid;
+    std::pair<c_map *, std::list<Item>> pk_pair;
+    std::list<Item> ls;
+    pk_pair = std::make_pair(poolid, ls);
+    pool_list.insert(pk_pair);
+    return pool;
 }
 
 void evict_LRU(c_map* in_pool_id) {
     c_map a = *in_pool_id;
-    int16_t max = 0, oldest_key;
-    std::pair<int16_t, std::any> oldest;
-
-    // searching for the oldest member ( find a better soln)
-
-    for (auto &i: a) {
-        oldest = i.second;
-        if (oldest.first > max) {
-            max = oldest.first;
-            oldest_key = i.first;
-        }
-    }
-    a.erase(oldest_key);
+    auto it = pool_list.find(in_pool_id);
+    std::list<Item> ls = it->second;
+    ls.pop_back();
+    //int16_t max = 0, oldest_key;
+    /* searching for the oldest member ( find a better soln)
+    auto oldest = std::max_element(a.begin(), a.end(),
+                     [](auto x, auto y) { return x.second.first <  y.second.first; });
+    a.erase(oldest->first);
+     */
 }
 
 // creating an item to be assigned in a pool, the function initializes the parameters and returns the object of "Item" type
 
-Item create_item(std::any val, c_map *pid) {
+Item create_item(std::string Key) {
     Item i;
+    i.Key = Key;
+    i.d = 0;
     i.age = 0;
-    i.d = val;
-    i.Key = kit;
-    i.pool = pid;
-    kit += 1;
     return i;
 }
 
-void increase_age(c_map *pool) {
+/*void increase_age(c_map *pool) {
     c_map map = *pool;
     for (auto i: map) {      // increasing the age
         i.second.first += 1;
     }
 }
+ */
 //checks if map is empty before, to be added, option to choose eviction policy.
 
-void insert_key(Item i) {
-    c_map map = *i.pool;
-    std::pair<int16_t, std::any> val;
-    std::pair<int16_t, std::pair<int16_t, std::any>> kv_pair;
-    val.first = i.age;
-    val.second = i.d;
-    kv_pair.first = i.Key;
-    kv_pair.second = val;
+void insert_item(Item i, c_map *poolid) {
+    c_map map = *poolid;
+    auto it = pool_list.find(poolid);
+    std::list<Item> ls = it->second;
+    //val.first = i.age;
+    //val.second = i.d;
     if (map.size() >= 256) {
-        evict_LRU(i.pool);
+        evict_LRU(poolid);
     }
-    map.insert(kv_pair);
-    increase_age(i.pool);
+    ls.push_front(i);
+    Item *n = &i;
+    map.insert({i.Key, n});
+    //increase_age(i.pool);
 }
 
 //finding an element
+void update_pos(c_map *poolid, Item i) {
+    auto it = pool_list.find(poolid);
+    Item a = i;
+    std::list<Item> ls = it->second;
+    std::list<Item>::iterator it1 = std::find(ls.begin(), ls.end(), i);
+}
 
-std::any find(c_map *pool, int16_t Key) {
+Item find(c_map *pool, std::string Key) {
     c_map map = *pool;
     auto f = map.find(Key);
-    std::pair<int16_t, std::any> d_pair = f->second;
-    increase_age(pool);
-    return d_pair.second;
+    Item i = *f->second;
+    update_pos(pool, i);
+    return i;
 }
 
 void change_value(int16_t key, std::any new_d, c_map *pool) {
     c_map map = *pool;
-    map.at(key).second = new_d;
-    increase_age(pool);
+
 }
 
 void flush(c_map *pool) {
